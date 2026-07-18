99769e7536ec52cc2627038dd7264436a43f22f5
ArulSelvar S <152908580+Arul-selvar-s@users.noreply.github.com>
2026-07-19 00:56:35 +0530

    Add provideNoteDao method to AppModule

--- app/src/main/java/com/compass/diary/util/AppModule.kt
+++ app/src/main/java/com/compass/diary/util/AppModule.kt
@@ -25,4 +25,5 @@ object AppModule {
     @Provides fun provideDrawingDao(db: AppDatabase): DrawingDao = db.drawingDao()
     @Provides fun provideSongDao(db: AppDatabase): SongDao = db.songDao()
     @Provides fun provideVoiceMessageDao(db: AppDatabase): VoiceMessageDao = db.voiceMessageDao()
+    @Provides fun provideNoteDao(db: AppDatabase): NoteDao = db.noteDao()
 }
